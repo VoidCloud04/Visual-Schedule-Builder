81c8869db2b8fe372624cf0673bbcd736ba1ab67
VoidCloud04 <MrBacon470@outlook.com>
2026-07-03 11:45:14 -0500

    purge and redo, using own UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<style lang="scss">
+    @use '$lib/styles/global' as *;
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: [
-        vitePreprocess({
-            style: true,
-            script: false
-        })
-    ]
-}
+    preprocess: [vitePreprocess({ style: true, script: false })],
+    kit: { adapter: adapter({
+        pages: 'build',
+        assets: 'build',
+        fallback: '404.html'
+    }),
+    paths: {
+        base: dev ? '' : '/Visual-Schedule-Builder'
+    }
+    }
+};
 
 export default config;
